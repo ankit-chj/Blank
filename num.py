f8a41bc65327b2f2f51cf5963e9aa838deb990a8
Ankit <ankitdoha111@gmail.com>
2026-01-17 22:53:35 +0530

    second

--- num.py
+++ num.py
@@ -1,1 +1,10 @@
+import numpy as np
 print("hello world")
+def add_arrays(arr1, arr2):
+    return np.add(arr1, arr2)
+
+arr1 = [1, 2, 3]
+arr2 = [4, 5, 6]
+result = add_arrays(arr1, arr2)
+
+print("Result:", result)
